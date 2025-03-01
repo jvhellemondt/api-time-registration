4dbee5b5eca09de054b1725476088f3f3eeeec7f
Jens van Hellemondt <jens@invictus.codes>
2025-03-01 23:09:57 +0100

    feat(TimeEntry): should contain an user id

--- src/domain/TimeEntry/TimeEntry.spec.ts
+++ src/domain/TimeEntry/TimeEntry.spec.ts
@@ -1,7 +1,16 @@
+import { randomUUID } from 'node:crypto'
 import { TimeEntry } from './TimeEntry'
 
 describe('timeEntry', () => {
   it('should be defined', () => {
     expect(TimeEntry).toBeDefined()
+  })
+
+  it('should contain an user id', () => {
+    const aggregateId = randomUUID()
+    const props = { userId: randomUUID() }
+    const timeEntry = TimeEntry.create(props, aggregateId)
+    expect(timeEntry.id).toBe(aggregateId)
+    expect(timeEntry.props.userId).toBe(props.userId)
   })
 })
--- src/domain/TimeEntry/TimeEntry.ts
+++ src/domain/TimeEntry/TimeEntry.ts
@@ -1,1 +1,18 @@
-export class TimeEntry { }
+import type { DomainEvent } from '@jvhellemondt/crafts-and-arts.ts'
+import type { UUID } from 'node:crypto'
+import { AggregateRoot } from '@jvhellemondt/crafts-and-arts.ts'
+
+interface TimeEntryProps {
+  userId: UUID
+}
+
+export class TimeEntry extends AggregateRoot<TimeEntryProps> {
+  static create(props: TimeEntryProps, id: UUID) {
+    const aggregate = new this(props, id)
+    return aggregate
+  }
+
+  protected _applyEvent(_event: DomainEvent<unknown>): void {
+    throw new Error('Method not implemented.')
+  }
+}
