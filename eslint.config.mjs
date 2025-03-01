925c7adf9ad6dacb7f74ff1cfcf8369d24c64233
Jens van Hellemondt <jens@invictus.codes>
2025-03-01 22:17:22 +0100

    chore: add eslintconfig and lint

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import antfu from '@antfu/eslint-config'
+import globals from 'globals'
+
+export default antfu(
+  {
+    typescript: {
+      tsconfigPath: 'tsconfig.json',
+    },
+  },
+  { languageOptions: { globals: globals.node } },
+  {
+    rules: {
+      'no-console': 'warn',
+      'unused-imports/no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_[^_].*$|^_$',
+          varsIgnorePattern: '^_[^_].*$|^_$',
+          caughtErrorsIgnorePattern: '^_[^_].*$|^_$',
+        },
+      ],
+    },
+  },
+)
