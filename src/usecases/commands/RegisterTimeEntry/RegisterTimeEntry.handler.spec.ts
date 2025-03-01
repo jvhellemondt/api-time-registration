5368e90ac49ef2a64dca86a3b951cd32e48f7a96
Jens van Hellemondt <jens@invictus.codes>
2025-03-01 23:39:56 +0100

    feat(RegisterTimeEntry): should return a 'not implemented' error upon execution

--- src/usecases/commands/RegisterTimeEntry/RegisterTimeEntry.handler.spec.ts
+++ src/usecases/commands/RegisterTimeEntry/RegisterTimeEntry.handler.spec.ts
@@ -1,7 +1,34 @@
+import { randomUUID } from 'node:crypto'
+import { InMemoryTimeEntryRepository } from '@/repositories/TimeEntryRepository/implementations/InMemoryTimeEntry.implementation'
+import { CommandBus, EventBus, InMemoryEventStore } from '@jvhellemondt/crafts-and-arts.ts'
+import { subDays } from 'date-fns'
+import { RegisterTimeEntryCommand } from './RegisterTimeEntry.command'
 import { RegisterTimeEntryHandler } from './RegisterTimeEntry.handler'
 
 describe('registerTimeEntryHandler', () => {
   it('should be defined', () => {
     expect(RegisterTimeEntryHandler).toBeDefined()
+  })
+
+  it('should return a "not implemented" error upon execution', async () => {
+    // arrange
+    const eventBus = new EventBus()
+    const eventStore = new InMemoryEventStore(eventBus)
+    const repository = new InMemoryTimeEntryRepository(eventStore)
+    const handler = new RegisterTimeEntryHandler(repository)
+    const commandBus = new CommandBus()
+    commandBus.register(RegisterTimeEntryCommand, handler)
+
+    const userId = randomUUID()
+    const endTime = new Date()
+    const startTime = subDays(endTime, 3)
+
+    const command = new RegisterTimeEntryCommand({ userId, startTime, endTime })
+
+    // act
+    const execute = () => handler.execute(command)
+
+    // assert
+    expect(execute).rejects.toThrow()
   })
 })
--- src/usecases/commands/RegisterTimeEntry/RegisterTimeEntry.handler.ts
+++ src/usecases/commands/RegisterTimeEntry/RegisterTimeEntry.handler.ts
@@ -1,1 +1,13 @@
-export class RegisterTimeEntryHandler { }
+import type { RegisterTimeEntryCommand } from './RegisterTimeEntry.command'
+import { randomUUID } from 'node:crypto'
+import { TimeEntry } from '@/domain/TimeEntry/TimeEntry'
+import { CommandHandler } from '@jvhellemondt/crafts-and-arts.ts'
+
+export class RegisterTimeEntryHandler extends CommandHandler<RegisterTimeEntryCommand> {
+  async execute(command: RegisterTimeEntryCommand): Promise<{ id: string }> {
+    const aggregateId = randomUUID()
+    const aggregate = TimeEntry.create(command.payload, aggregateId)
+    await this.repository.store(aggregate)
+    return { id: aggregateId }
+  }
+}
