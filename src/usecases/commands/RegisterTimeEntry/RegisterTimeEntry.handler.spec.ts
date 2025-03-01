6ca33e806b96599bddfa96d2cf4e1f3be24b6707
Jens van Hellemondt <jens@invictus.codes>
2025-03-01 22:37:34 +0100

    feat(RegisterTimeEntry): handler should be defined

---
+++ src/usecases/commands/RegisterTimeEntry/RegisterTimeEntry.handler.spec.ts
@@ -0,0 +1,7 @@
+import { RegisterTimeEntryHandler } from './RegisterTimeEntry.handler'
+
+describe('registerTimeEntryHandler', () => {
+  it('should be defined', () => {
+    expect(RegisterTimeEntryHandler).toBeDefined()
+  })
+})
