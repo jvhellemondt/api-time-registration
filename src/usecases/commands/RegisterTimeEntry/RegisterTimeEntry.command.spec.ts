9c0e2102aad3910c9f6c4454e72db80a4ae26fff
Jens van Hellemondt <jens@invictus.codes>
2025-03-01 22:32:14 +0100

    feat(RegisterTimeEntry): command should contain the right payload

--- src/usecases/commands/RegisterTimeEntry/RegisterTimeEntry.command.spec.ts
+++ src/usecases/commands/RegisterTimeEntry/RegisterTimeEntry.command.spec.ts
@@ -1,8 +1,22 @@
+import { randomUUID } from 'node:crypto'
+import { subDays } from 'date-fns'
 import { describe, expect, it } from 'vitest'
 import { RegisterTimeEntryCommand } from './RegisterTimeEntry.command'
 
 describe('registerTimeEntryCommand', () => {
   it('should be defined', () => {
     expect(RegisterTimeEntryCommand).toBeDefined()
+  })
+
+  it('should contain the right payload', () => {
+    const userId = randomUUID()
+    const startTime = new Date()
+    const endTime = subDays(startTime, 3)
+
+    const command = new RegisterTimeEntryCommand({ userId, startTime, endTime })
+
+    expect(command.payload.userId).toBe(userId)
+    expect(command.payload.startTime).toEqual(startTime)
+    expect(command.payload.endTime).toEqual(endTime)
   })
 })
--- src/usecases/commands/RegisterTimeEntry/RegisterTimeEntry.command.ts
+++ src/usecases/commands/RegisterTimeEntry/RegisterTimeEntry.command.ts
@@ -1,1 +1,9 @@
-export class RegisterTimeEntryCommand { }
+import { Command } from '@jvhellemondt/crafts-and-arts.ts'
+
+export interface RegisterTimeEntryCommandProps {
+  userId: string
+  startTime: Date
+  endTime: Date
+}
+
+export class RegisterTimeEntryCommand extends Command<RegisterTimeEntryCommandProps> { }
