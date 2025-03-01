f84527f0a061c00800fef81989f8429a531aeb65
Jens van Hellemondt <jens@invictus.codes>
2025-03-01 22:18:52 +0100

    feat(RegisterTimeEntry): command should be defined

---
+++ src/usecases/commands/RegisterTimeEntry/RegisterTimeEntry.command.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import { RegisterTimeEntryCommand } from './RegisterTimeEntry.command'
+
+describe('registerTimeEntryCommand', () => {
+  it('should be defined', () => {
+    expect(RegisterTimeEntryCommand).toBeDefined()
+  })
+})
