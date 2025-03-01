15b830504663165648a99abef9442e965e347308
Jens van Hellemondt <jens@invictus.codes>
2025-03-01 23:28:49 +0100

    feat(TimeEntryRegistered): should be defined

---
+++ src/domain/events/TimeEntryRegistered.event.spec.ts
@@ -0,0 +1,7 @@
+import { TimeEntryRegistered } from './TimeEntryRegistered.event'
+
+describe('timeEntryRegistered Event', () => {
+  it('should be defined', () => {
+    expect(TimeEntryRegistered).toBeDefined()
+  })
+})
