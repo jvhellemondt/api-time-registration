8eb99acf6a50542602446c93f5158d618e2afd3f
Jens van Hellemondt <jens@invictus.codes>
2025-03-21 23:32:59 +0100

    test(TimeEntryRepository): should be defined

---
+++ src/repositories/TimeEntryRepository/implementations/InMemoryTimeEntry.implementation.spec.ts
@@ -0,0 +1,7 @@
+import { InMemoryTimeEntryRepository } from './InMemoryTimeEntry.implementation'
+
+describe('inMemoryTimeEntryRepository', () => {
+  it('should be defined', () => {
+    expect(InMemoryTimeEntryRepository).toBeDefined()
+  })
+})
