b2204059078c1ea1f23fdabc135cf86dae64a586
Jens van Hellemondt <jens@invictus.codes>
2025-03-21 23:46:47 +0100

    test(TimeEntryRepository): should return the aggregate when loading an existing time entry

--- src/repositories/TimeEntryRepository/implementations/InMemoryTimeEntry.implementation.spec.ts
+++ src/repositories/TimeEntryRepository/implementations/InMemoryTimeEntry.implementation.spec.ts
@@ -1,6 +1,9 @@
+import type { TimeEntryProps } from '@/domain/TimeEntry/TimeEntry'
 import type { EventStore } from '@jvhellemondt/crafts-and-arts.ts'
 import { randomUUID } from 'node:crypto'
+import { TimeEntry } from '@/domain/TimeEntry/TimeEntry'
 import { EventBus, InMemoryEventStore } from '@jvhellemondt/crafts-and-arts.ts'
+import { subMinutes } from 'date-fns'
 import { InMemoryTimeEntryRepository } from './InMemoryTimeEntry.implementation'
 
 describe('inMemoryTimeEntryRepository', () => {
@@ -21,5 +24,20 @@ describe('inMemoryTimeEntryRepository', () => {
   it('should return an error when loading a non-existent time entry', async () => {
     const aggregateId = randomUUID()
     expect(() => repository.load(aggregateId)).rejects.toThrow()
+  })
+
+  it('should return the aggregate when loading an existing time entry', async () => {
+    const aggregateId = randomUUID()
+    const userId = randomUUID()
+    const props: TimeEntryProps = {
+      userId,
+      startTime: subMinutes(new Date(), 120),
+      endTime: subMinutes(new Date(), 60),
+    }
+    const aggregate = TimeEntry.create(props, aggregateId)
+    repository.store(aggregate)
+
+    const rehydratedAggregate = await repository.load(aggregateId)
+    expect(rehydratedAggregate.props).toBe(props)
   })
 })
