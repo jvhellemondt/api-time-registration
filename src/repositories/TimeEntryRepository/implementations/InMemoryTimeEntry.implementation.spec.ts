d3d8198f4c309e65890c0524b0e79cc63f2fd4f1
Jens van Hellemondt <jens@invictus.codes>
2025-03-21 23:41:57 +0100

    test(TimeEntryRepository): should return an error when loading a non-existent time entry

--- src/repositories/TimeEntryRepository/implementations/InMemoryTimeEntry.implementation.spec.ts
+++ src/repositories/TimeEntryRepository/implementations/InMemoryTimeEntry.implementation.spec.ts
@@ -1,7 +1,25 @@
+import type { EventStore } from '@jvhellemondt/crafts-and-arts.ts'
+import { randomUUID } from 'node:crypto'
+import { EventBus, InMemoryEventStore } from '@jvhellemondt/crafts-and-arts.ts'
 import { InMemoryTimeEntryRepository } from './InMemoryTimeEntry.implementation'
 
 describe('inMemoryTimeEntryRepository', () => {
+  let eventBus: EventBus
+  let eventStore: EventStore
+  let repository: InMemoryTimeEntryRepository
+
+  beforeEach(() => {
+    eventBus = new EventBus()
+    eventStore = new InMemoryEventStore(eventBus)
+    repository = new InMemoryTimeEntryRepository(eventStore)
+  })
+
   it('should be defined', () => {
     expect(InMemoryTimeEntryRepository).toBeDefined()
+  })
+
+  it('should return an error when loading a non-existent time entry', async () => {
+    const aggregateId = randomUUID()
+    expect(() => repository.load(aggregateId)).rejects.toThrow()
   })
 })
