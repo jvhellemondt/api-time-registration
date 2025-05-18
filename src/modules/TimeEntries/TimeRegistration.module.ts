7fa5c300fc888652779f54382151cf297dc72b25
Jens van Hellemondt <jens@invictus.codes>
2025-05-18 16:41:32 +0200

    refactor: move subscribers to usecases

--- src/modules/TimeEntries/TimeRegistration.module.ts
+++ src/modules/TimeEntries/TimeRegistration.module.ts
@@ -1,8 +1,8 @@
 import type { CommandBus, Database, EventBus, Module, QueryBus } from '@jvhellemondt/arts-and-crafts.ts'
 import type { TimeEntryRepository } from './repositories/TimeEntryRepository/TimeEntryRepository'
-import { AfterTimeEntryRegistered } from './subscribers/afterTimeEntryRegistered/afterTimeEntryRegistered'
 import { RegisterTimeEntryHandler } from './usecases/commands/RegisterTimeEntry/RegisterTimeEntry.handler'
 import { ListTimeEntriesByUserIdHandler } from './usecases/queries/ListTimeEntriesByUserId/ListTimeEntriesByUserId.handler'
+import { AfterTimeEntryRegistered } from './usecases/subscribers/afterTimeEntryRegistered/afterTimeEntryRegistered'
 
 export class TimeRegistrationModule implements Module {
   constructor(
