7d38e71f812427621b1517ca3d0df02356641c4f
Jens van Hellemondt <jens@invictus.codes>
2025-05-18 12:30:22 +0200

    feat: afterTimeEntryRegistered should implement ProjectionHandler

--- src/modules/TimeEntries/subscribers/afterTimeEntryRegistered/afterTimeEntryRegistered.spec.ts
+++ src/modules/TimeEntries/subscribers/afterTimeEntryRegistered/afterTimeEntryRegistered.spec.ts
@@ -1,7 +1,23 @@
+import type { Database } from '@jvhellemondt/arts-and-crafts.ts'
+import { EventBus, InMemoryDatabase } from '@jvhellemondt/arts-and-crafts.ts'
 import { AfterTimeEntryRegistered } from './afterTimeEntryRegistered'
 
 describe('afterTimeEntryRegistered subscriber', () => {
+  let eventBus: EventBus
+  let database: Database
+
+  beforeEach(() => {
+    eventBus = new EventBus()
+    database = new InMemoryDatabase()
+  })
+
   it('should be defined', () => {
     expect(AfterTimeEntryRegistered).toBeDefined()
+  })
+
+  it('should implement ProjectionHandler', () => {
+    const handler = new AfterTimeEntryRegistered(eventBus, database)
+    expect(handler.start).toThrow()
+    expect(handler.update).toThrow()
   })
 })
--- src/modules/TimeEntries/subscribers/afterTimeEntryRegistered/afterTimeEntryRegistered.ts
+++ src/modules/TimeEntries/subscribers/afterTimeEntryRegistered/afterTimeEntryRegistered.ts
@@ -1,3 +1,12 @@
-export class AfterTimeEntryRegistered {
+import type { BaseEvent } from '@jvhellemondt/arts-and-crafts.ts/src/infrastructure/EventBus/Event'
+import { ProjectionHandler } from '@jvhellemondt/arts-and-crafts.ts'
 
+export class AfterTimeEntryRegistered extends ProjectionHandler {
+  start(): void {
+    throw new Error('Method not implemented.')
+  }
+
+  update(_event: BaseEvent): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
 }
