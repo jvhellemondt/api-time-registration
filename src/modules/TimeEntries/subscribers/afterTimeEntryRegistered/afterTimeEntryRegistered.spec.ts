26a5c5be2049ae90b7b25398427a4a299165a2bc
Jens van Hellemondt <jens@invictus.codes>
2025-05-18 12:17:14 +0200

    feat: afterTimeEntryRegistered should be defined

---
+++ src/modules/TimeEntries/subscribers/afterTimeEntryRegistered/afterTimeEntryRegistered.spec.ts
@@ -0,0 +1,7 @@
+import { AfterTimeEntryRegistered } from './afterTimeEntryRegistered'
+
+describe('afterTimeEntryRegistered subscriber', () => {
+  it('should be defined', () => {
+    expect(AfterTimeEntryRegistered).toBeDefined()
+  })
+})
