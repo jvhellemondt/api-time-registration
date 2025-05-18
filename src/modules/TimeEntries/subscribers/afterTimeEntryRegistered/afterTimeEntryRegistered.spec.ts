98ac3d8b5518798483850d9130f5949c530e88a4
Jens van Hellemondt <jens@invictus.codes>
2025-05-18 13:18:47 +0200

    feat: afterTimeEntryRegistered should add the TimeEntry to the database

--- src/modules/TimeEntries/subscribers/afterTimeEntryRegistered/afterTimeEntryRegistered.spec.ts
+++ src/modules/TimeEntries/subscribers/afterTimeEntryRegistered/afterTimeEntryRegistered.spec.ts
@@ -1,14 +1,29 @@
-import type { Database } from '@jvhellemondt/arts-and-crafts.ts'
-import { EventBus, InMemoryDatabase } from '@jvhellemondt/arts-and-crafts.ts'
+import type { Database, DomainEvent, FilledArray } from '@jvhellemondt/arts-and-crafts.ts'
+import type { TimeEntryProps } from '@/TimeEntries/domain/TimeEntry/TimeEntry'
+import { randomUUID } from 'node:crypto'
+import { EventBus, InMemoryDatabase, Specification } from '@jvhellemondt/arts-and-crafts.ts'
+import { subHours } from 'date-fns'
+import { TimeEntryRegistered } from '@/TimeEntries/domain/events/TimeEntryRegistered.event'
 import { AfterTimeEntryRegistered } from './afterTimeEntryRegistered'
 
 describe('afterTimeEntryRegistered subscriber', () => {
+  let aggregateId: string
+  let props: TimeEntryProps
   let eventBus: EventBus
   let database: Database
+  let event: DomainEvent
 
   beforeEach(() => {
+    aggregateId = randomUUID()
     eventBus = new EventBus()
     database = new InMemoryDatabase()
+
+    props = {
+      userId: randomUUID(),
+      startTime: subHours(new Date(), 3),
+      endTime: new Date(),
+    }
+    event = TimeEntryRegistered(aggregateId, props)
   })
 
   it('should be defined', () => {
@@ -18,6 +33,31 @@ describe('afterTimeEntryRegistered subscriber', () => {
   it('should implement ProjectionHandler', () => {
     const handler = new AfterTimeEntryRegistered(eventBus, database)
     expect(handler.start).toThrow()
-    expect(handler.update).toThrow()
+  })
+
+  it('should add the TimeEntry to the database', async () => {
+    const handler = new AfterTimeEntryRegistered(eventBus, database)
+    handler.start()
+
+    class GetTimeEntryById extends Specification<DomainEvent<TimeEntryProps>> {
+      constructor(
+        private readonly id: string,
+      ) {
+        super()
+      }
+
+      isSatisfiedBy(candidate: DomainEvent<TimeEntryProps>): boolean {
+        return candidate.aggregateId === this.id
+      }
+
+      toQuery(): FilledArray {
+        return [{ id: this.id }]
+      }
+    }
+    const spec = new GetTimeEntryById(aggregateId)
+    await eventBus.publish(event)
+
+    const results = await database.query('time-entries', spec)
+    expect(results[0]).toStrictEqual({ id: aggregateId, ...props })
   })
 })
--- src/modules/TimeEntries/subscribers/afterTimeEntryRegistered/afterTimeEntryRegistered.ts
+++ src/modules/TimeEntries/subscribers/afterTimeEntryRegistered/afterTimeEntryRegistered.ts
@@ -1,12 +1,19 @@
-import type { BaseEvent } from '@jvhellemondt/arts-and-crafts.ts/src/infrastructure/EventBus/Event'
-import { ProjectionHandler } from '@jvhellemondt/arts-and-crafts.ts'
+import type { DatabaseRecord, DomainEvent } from '@jvhellemondt/arts-and-crafts.ts'
+import type { TimeEntryProps } from '@/TimeEntries/domain/TimeEntry/TimeEntry'
+import { Operation, ProjectionHandler } from '@jvhellemondt/arts-and-crafts.ts'
 
 export class AfterTimeEntryRegistered extends ProjectionHandler {
   start(): void {
-    throw new Error('Method not implemented.')
+    this.eventBus.subscribe(this)
   }
 
-  update(_event: BaseEvent): Promise<void> {
-    throw new Error('Method not implemented.')
+  async update(event: DomainEvent<TimeEntryProps>): Promise<void> {
+    switch (event.type) {
+      case 'TimeEntryRegistered': {
+        const payload: DatabaseRecord = { id: event.aggregateId, ...event.payload }
+        await this.database.execute('time-entries', { operation: Operation.CREATE, payload })
+        break
+      }
+    }
   }
 }
