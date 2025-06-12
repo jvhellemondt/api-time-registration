6cc5eaff4b94557141266dcd275596b45e2eee44
jvhellemondt <j62676@eon.com>
2025-06-12 12:34:09 +0200

    feat: use terminate in env setup

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,12 +1,12 @@
 import process from 'node:process'
 import colors from 'colors/safe'
 
-import { fail } from '@/shared/utils/fail/fail'
 import { invariant } from '@/shared/utils/invariant/invariant'
+import { terminate } from '@/shared/utils/terminate/terminate'
 import packageJson from '../../package.json'
 
 function requireFromEnv(key: string) {
-  invariant(!!process.env[key], fail(new Error(`${colors.red('[APP ERROR] Missing env variable:')} ${key}`)))
+  invariant(!!process.env[key], terminate(new Error(`${colors.red('[APP ERROR] Missing env variable:')} ${key}`)))
   return process.env[key]
 }
 
