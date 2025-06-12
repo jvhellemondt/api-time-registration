17c4eda7143a80c8818ed423463c58c253baa081
jvhellemondt <j62676@eon.com>
2025-06-12 12:58:25 +0200

    chore: suppress console output during testing

--- src/shared/utils/terminate/terminate.spec.ts
+++ src/shared/utils/terminate/terminate.spec.ts
@@ -2,6 +2,10 @@ import process from 'node:process'
 import { terminate } from './terminate'
 
 describe('terminate (with Error parameter)', () => {
+  // suppress the error logging to prevent console output during tests
+  const logSpy = vitest.spyOn(console, 'error').mockImplementation(() => {
+    return '' as never
+  })
   const exitSpy = vitest.spyOn(process, 'exit').mockImplementation(() => {
     throw new Error('Mocked error')
   })
@@ -10,6 +14,7 @@ describe('terminate (with Error parameter)', () => {
     const errorMessage = 'This is an expected error message!'
     const errorInstance = new Error(errorMessage)
     expect(terminate(errorInstance)).toThrow()
+    expect(logSpy).toHaveBeenCalledOnce()
     expect(exitSpy).toHaveBeenCalledOnce()
   })
 })
