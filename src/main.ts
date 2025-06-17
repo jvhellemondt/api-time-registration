6f9c3590270e4d7efee56c7e94b4d902df9e595e
jvhellemondt <j62676@eon.com>
2025-06-17 11:58:15 +0200

    feat: have server running through effect

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
+import { Effect } from 'effect'
 import { start } from '@/config/bootstrap'
 
-start()
+function startServer() {
+  return Effect.try({
+    try: start,
+    catch: unknownError =>
+      new Error(`Server could not start. Something went wrong ${unknownError}`),
+  })
+}
+
+Effect.runSync(startServer())
