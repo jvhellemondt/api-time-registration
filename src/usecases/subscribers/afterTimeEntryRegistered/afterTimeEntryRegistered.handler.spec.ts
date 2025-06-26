be84a0931976c48f1451b3586cc0ad6113b46739
Jens van Hellemondt <jens@invictus.codes>
2025-06-26 19:55:48 +0200

    feat: afterTimeEntryRegisteredHandler should be defined

---
+++ src/usecases/subscribers/afterTimeEntryRegistered/afterTimeEntryRegistered.handler.spec.ts
@@ -0,0 +1,7 @@
+import { AfterTimeEntryRegisteredHandler } from './afterTimeEntryRegistered.handler'
+
+describe('afterTimeEntryRegisteredHandler', () => {
+  it('should be defined', () => {
+    expect(AfterTimeEntryRegisteredHandler).toBeDefined()
+  })
+})
