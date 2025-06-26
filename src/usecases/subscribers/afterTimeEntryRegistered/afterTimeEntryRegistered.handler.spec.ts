1ad34fddaf14b7f2e1bfeaf1c7c8e31565877162
Jens van Hellemondt <jens@invictus.codes>
2025-06-26 20:11:02 +0200

    feat: afterTimeEntryRegisteredHandler should implement the projection handler interface

--- src/usecases/subscribers/afterTimeEntryRegistered/afterTimeEntryRegistered.handler.spec.ts
+++ src/usecases/subscribers/afterTimeEntryRegistered/afterTimeEntryRegistered.handler.spec.ts
@@ -4,4 +4,10 @@ describe('afterTimeEntryRegisteredHandler', () => {
   it('should be defined', () => {
     expect(AfterTimeEntryRegisteredHandler).toBeDefined()
   })
+
+  it('should implement the projection handler interface', () => {
+    const handler = new AfterTimeEntryRegisteredHandler()
+    expect(() => handler.handle({} as any)).rejects.toThrow()
+    expect(() => handler.start()).toThrow()
+  })
 })
--- src/usecases/subscribers/afterTimeEntryRegistered/afterTimeEntryRegistered.handler.ts
+++ src/usecases/subscribers/afterTimeEntryRegistered/afterTimeEntryRegistered.handler.ts
@@ -1,1 +1,12 @@
-export class AfterTimeEntryRegisteredHandler { }
+import type { DomainEvent, ProjectionHandler } from '@jvhellemondt/arts-and-crafts.ts'
+import type { TimeEntryEvent } from '@/domain/TimeEntry/TimeEntry.decider'
+
+export class AfterTimeEntryRegisteredHandler implements ProjectionHandler<TimeEntryEvent> {
+  start() {
+    throw new Error('Method not implemented.')
+  }
+
+  async handle(_anEvent: DomainEvent<{ userId: string, startTime: string, endTime: string }>) {
+    throw new Error('Method not implemented.')
+  }
+}
