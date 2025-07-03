e7b3a02ecd2ee2ef5e16d00da4e8e95baf82cb93
Jens van Hellemondt <jens@invictus.codes>
2025-07-03 19:41:36 +0200

    feat(TimeEntriesProjection): register to eventbus

--- src/TimeRegistration.module.ts
+++ src/TimeRegistration.module.ts
@@ -1,6 +1,7 @@
 import type { CommandBus, Database, EventBus, Module, QueryBus, Repository } from '@jvhellemondt/arts-and-crafts.ts'
 import type { TimeEntryEvent } from './domain/TimeEntry/TimeEntry.decider'
 import { RegisterTimeEntryHandler } from '@/usecases/commands/RegisterTimeEntry/RegisterTimeEntry.handler.ts'
+import { TimeEntriesProjectionHandler } from './usecases/projectors/TimeEntriesProjection/TimeEntriesProjection.handler'
 
 export class TimeRegistrationModule implements Module {
   constructor(
@@ -14,5 +15,7 @@ export class TimeRegistrationModule implements Module {
 
   registerModule() {
     this.commandBus.register('RegisterTimeEntry', new RegisterTimeEntryHandler(this.repository))
+
+    new TimeEntriesProjectionHandler(this.eventBus, this.database).start()
   }
 };
