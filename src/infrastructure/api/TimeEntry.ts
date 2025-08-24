b93f7e43e754c2d647dbbdfe8cfb7d5e9c0e8ca6
Jens van Hellemondt <jens@invictus.codes>
2025-08-24 10:28:47 +0200

    feat(api): traffic logger middleware

--- src/infrastructure/api/TimeEntry.ts
+++ src/infrastructure/api/TimeEntry.ts
@@ -1,6 +1,7 @@
 import type { TimeRegistrationModule } from '@/TimeRegistration.module'
 import { Hono } from 'hono'
 import { v7 as uuidv7 } from 'uuid'
+import { trafficLoggerMiddleware } from '@/infrastructure/api/middleware/trafficLogger.middleware.ts'
 import { symListTimeEntriesDirective, symRepository } from '@/TimeRegistration.module'
 import { createRegisterTimeEntryCommand } from '@/usecases/commands/RegisterTimeEntry/RegisterTimeEntry.command'
 import { RegisterTimeEntryHandler } from '@/usecases/commands/RegisterTimeEntry/RegisterTimeEntry.handler'
@@ -11,6 +12,8 @@ import { createListTimeEntriesByUserIdQuery } from '@/usecases/queries/ListTimeE
 
 export default function TimeEntryApi(module: TimeRegistrationModule) {
   return new Hono()
+    .use(trafficLoggerMiddleware)
+
     .get('/health', c => c.text('HEALTH OK'))
 
     .get('/list-time-entries', async (c) => {
---
+++ src/infrastructure/api/middleware/trafficLogger.middleware.ts
@@ -0,0 +1,21 @@
+import type { Context, Next } from 'hono'
+
+export async function trafficLoggerMiddleware(c: Context, next: Next) {
+  const start = Date.now()
+
+  await next()
+
+  const latency = Date.now() - start
+  const method = c.req.method
+  const url = c.req.url
+  const path = c.req.path
+  const status = c.res.status
+
+  // eslint-disable-next-line no-console
+  console.group(`${method} ${path} ${status}`)
+  // eslint-disable-next-line no-console
+  console.info(`[${new Date().toISOString()}] ${latency}ms - ${url}`)
+  // eslint-disable-next-line no-console
+  console.groupEnd()
+  c.header('X-Response-Time', `${latency}ms`)
+}
